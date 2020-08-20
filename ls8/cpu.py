7ea9b8629815e29d7b5108b12862efe90bf2dd2c
M Groesbeck <m.groesbeck@gmail.com>
2020-08-20 10:50:08 -0700

    Branch table implemented and tested on supported operations.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,10 +7,25 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
+        self.ram = []
+        self.running = False
         self.pc = 0
         self.reg = [0] * 8
         self.stack = [0] * 256
         self.reg[7] = 256
+        self.commands = {
+            0b00000001: self.halt,
+            0b01000101: self.push,
+            0b01000110: self.pop,
+            0b01000111: self.prn,
+            0b10000010: self.ldi,
+            0b10100000: self.alu,
+            0b10100010: self.alu
+        }
+        self.com_args = {
+            0b10100000: "ADD",
+            0b10100010: "MUL"
+        }
 
     def load(self, filename):
         """Load a program into memory."""
@@ -24,15 +39,18 @@ class CPU:
         address = 0
         self.ram = [0] * len(program)
         for instruction in program:
-            # self.ram[address] = int(instruction,2)
             self.ram_write(address, int(instruction,2))
             address += 1
+        self.pc = 0
 
     def ram_read(self, i):
         return self.ram[i]
     
     def ram_write(self, i, v):
         self.ram[i] = v
+    
+    def halt(self):
+        self.running = False
     
     def push(self):
         if self.reg[7] > 0:
@@ -46,14 +64,27 @@ class CPU:
             self.reg[self.ram_read(self.pc + 1)] = self.stack[self.reg[7]]
             self.reg[7] += 1
             self.pc += 2
+    
+    def prn(self):
+        print(self.reg[self.ram[self.pc + 1]])
+        self.pc += 2
+    
+    def ldi(self):
+        self.reg[self.ram[self.pc+1]] = self.ram[self.pc+2]
+        self.pc += 3
 
     def alu(self, op, reg_a=None, reg_b=None):
         """ALU operations."""
-
+        if reg_a is None:
+            reg_a = self.ram[self.pc + 1]
+        if reg_b is None:
+            reg_b = self.ram[self.pc + 2]
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+            self.pc += 3
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
+            self.pc += 3
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -79,24 +110,10 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        running = True
-        while running and (self.pc < len(self.ram)):
+        self.running = True
+        while self.running and (self.pc < len(self.ram)):
             this_instr = self.ram_read(self.pc)
-            if this_instr == 0b00000001:
-                running = False
-            elif this_instr == 0b01000101:
-                self.push()
-            elif this_instr == 0b01000110:
-                self.pop()
-            elif this_instr == 0b01000111:
-                print(self.reg[self.ram[self.pc + 1]])
-                self.pc += 2
-            elif this_instr == 0b10000010:
-                self.reg[self.ram[self.pc+1]] = self.ram[self.pc+2]
-                self.pc += 3
-            elif this_instr == 0b10100000:
-                self.alu("ADD", self.ram[self.pc + 1], self.ram[self.pc + 2])
-                self.pc += 3
-            elif this_instr == 0b10100010:
-                self.alu("MUL", self.ram[self.pc + 1], self.ram[self.pc + 2])
-                self.pc += 3
+            if this_instr in self.com_args:
+                self.commands[this_instr](self.com_args[this_instr])
+            else:
+                self.commands[this_instr]()
